e0a541886ea522b9dbd787b88a25b8a20a97712c
Habibullah Howlader <habibullah.howlader@bjitacademy.com>
2023-05-17 10:02:27 +0600

    Spring Security
    Have completed basic task of Spring security

---
+++ src/main/java/com/bjitacademy/securityModule/entity/UserResponseEntity.java
@@ -0,0 +1,20 @@
+package com.bjitacademy.securityModule.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String firstName;
+   // private String lastName;
+    private String userName;
+    private String email;
+}
